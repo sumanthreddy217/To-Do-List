cf10789a6e7dfef8b89b4ae3ac659657eb9701b7
sumanthreddy217 <sumanthreddy.k861@gmail.com>
2025-07-02 20:44:11 +0530

    build files and updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
-import TaskItem from './components/taskItem'
+import { useState } from "react";
+import "./App.css";
+import TaskItem from "./components/taskItem";
 
 function App() {
-  const [newTask,setNewTask]=useState("");
+  const [newTask, setNewTask] = useState("");
 
-  const [myTasks,setMyTasks]=useState(()=>{
+  const [myTasks, setMyTasks] = useState(() => {
     const storedTasks = localStorage.getItem("myTasks");
     return storedTasks ? JSON.parse(storedTasks) : [];
   });
@@ -15,25 +15,25 @@ function App() {
     return storedCompleted ? JSON.parse(storedCompleted) : [];
   });
 
-  function handleInput(e){
+  function handleInput(e) {
     let newValue = e.target.value;
     setNewTask(newValue);
   }
 
   function addTask() {
     if (newTask.trim() === "") return;
-    setMyTasks(prev=>{
+    setMyTasks((prev) => {
       const updated = [...prev, newTask];
-      console.log(myTasks)
-      localStorage.setItem("myTasks",JSON.stringify(updated));
+      console.log(myTasks);
+      localStorage.setItem("myTasks", JSON.stringify(updated));
       return updated;
     });
     setNewTask("");
   }
-  
+
   function deleteTask(taskName) {
-    const updatedMyTasks = myTasks.filter(x => x !== taskName);
-    const updatedCompleted = completedTasks.filter(x => x !== taskName);
+    const updatedMyTasks = myTasks.filter((x) => x !== taskName);
+    const updatedCompleted = completedTasks.filter((x) => x !== taskName);
 
     setMyTasks(updatedMyTasks);
     setCompletedTasks(updatedCompleted);
@@ -43,8 +43,8 @@ function App() {
   }
 
   function completeTask(taskName) {
-    const updatedMyTasks = myTasks.filter(x => x !== taskName);
-    const completedTask = myTasks.find(x => x === taskName);
+    const updatedMyTasks = myTasks.filter((x) => x !== taskName);
+    const completedTask = myTasks.find((x) => x === taskName);
     const updatedCompleted = [...completedTasks, completedTask];
 
     setMyTasks(updatedMyTasks);
@@ -54,44 +54,72 @@ function App() {
     localStorage.setItem("completedTasks", JSON.stringify(updatedCompleted));
   }
 
-
   return (
-    <div className='main-body d-flex justify-content-center align-items-center'>
-      <div className='todo-list-mainDiv'>
+    <div className="main-body d-flex justify-content-center align-items-center">
+      <div className="todo-list-mainDiv">
         <h3>My To-Do List</h3>
 
         <div>
-          <div className='todo-task-input-div d-flex align-items-center justify-content-center'>
+          <div className="todo-task-input-div d-flex align-items-center justify-content-center">
             <div className="form-floating w-75">
-              <input type="text" className="form-control small-input" id="floatingInput" placeholder="To-Do Task" onChange={(e)=>{
-                handleInput(e)
-              }} value={newTask}/>
+              <input
+                type="text"
+                className="form-control small-input"
+                id="floatingInput"
+                placeholder="To-Do Task"
+                onChange={(e) => {
+                  handleInput(e);
+                }}
+                value={newTask}
+              />
               <label htmlFor="floatingInput">To-Do Task</label>
             </div>
-            <button className='btn btn-primary' id='add-button' onClick={()=>{addTask()}}> + </button>
+            <button
+              className="btn btn-primary"
+              id="add-button"
+              onClick={() => {
+                addTask();
+              }}
+            >
+              {" "}
+              +{" "}
+            </button>
           </div>
-          <h6> <u>Need To Completed</u> </h6>
-          <ul className='tasks-list'>
-            {
-              myTasks.map((task, index)=>
-                <TaskItem taskName={task} key={index} deleteTask={deleteTask} completeTask={completeTask} isCompleted={false}/>
-              )
-            }
+          <h6>
+            {" "}
+            <u>Need To Completed</u>{" "}
+          </h6>
+          <ul className="tasks-list">
+            {myTasks.map((task, index) => (
+              <TaskItem
+                taskName={task}
+                key={index}
+                deleteTask={deleteTask}
+                completeTask={completeTask}
+                isCompleted={false}
+              />
+            ))}
           </ul>
           <hr />
-          <h6> <u>Completed tasks</u> </h6>
-          <ul className='tasks-list'>
-            {
-              completedTasks.map((task, index)=>
-                <TaskItem taskName={task} key={index} deleteTask={deleteTask} completeTask={completeTask} isCompleted={true}/>
-              )
-            }
+          <h6>
+            {" "}
+            <u>Completed tasks</u>{" "}
+          </h6>
+          <ul className="tasks-list">
+            {completedTasks.map((task, index) => (
+              <TaskItem
+                taskName={task}
+                key={index}
+                deleteTask={deleteTask}
+                completeTask={completeTask}
+                isCompleted={true}
+              />
+            ))}
           </ul>
         </div>
-      
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
